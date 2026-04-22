c3ed918d0cf4888969389f63d11d38e78c4a1883
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-22 15:48:02 -0300

    Detail genre

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,12 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from .models import Genre
 
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         new_genre = Genre(name=data.get('name'))
@@ -19,4 +20,13 @@ def genre_view(request):
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
         return JsonResponse(
             data, safe=False
+        )
+    
+
+@csrf_exempt    
+def genre_detail_view(request, pk):
+    data = get_object_or_404(Genre, pk=pk)
+
+    return JsonResponse(
+            {"id": data.id, "name": data.name}, safe=False
         )
