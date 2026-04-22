35793a23242a8a7b8697a146e94b3ed233db4034
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-22 15:21:50 -0300

    Create genre

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,22 @@
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Genre
 
 
-# Create your views here.
+@csrf_exempt
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(
-        data, safe=False
-    )
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data.get('name'))
+        new_genre.save()
+        return JsonResponse(
+            {"id": new_genre.id, "name": new_genre.name}, safe=False, status=201
+        )
+
+    elif request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(
+            data, safe=False
+        )
