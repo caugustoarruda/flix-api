b4cb12f3b27413719f9c5b8a81aa8f5e919af113
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-23 17:25:17 -0300

    Change datail_view to RetrieveUpdateDestroyView rest_framework

--- genres/views.py
+++ genres/views.py
@@ -1,9 +1,5 @@
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-from .models import Genre
 from rest_framework import generics
+from .models import Genre
 from genres.serializers import GenreSerializer
 
   
@@ -12,23 +8,6 @@ class GenreCreateListView(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
     
 
-@csrf_exempt    
-def genre_detail_view(request, pk):
-    data = get_object_or_404(Genre, pk=pk)
-
-    if request.method == 'GET':
-        return JsonResponse(
-                {"id": data.id, "name": data.name}
-            )
-    elif request.method == 'PUT':
-        body = json.loads(request.body.decode('utf-8'))
-        data.name = body.get('name')
-        data.save()
-        return JsonResponse(
-                {"id": data.id, "name": data.name}
-            )
-    elif request.method == 'DELETE':
-        data.delete()
-        return JsonResponse(
-                {"message": "Genre deleted"}, status=204
-            )
+class RetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
