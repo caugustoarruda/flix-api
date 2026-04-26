c8503e4de8ce5923158c106168f9b3b14af935d3
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-26 15:56:59 -0300

    Complete CRUD movies

--- app/urls.py
+++ app/urls.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 
     path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-datail-view')
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-datail-view'),
+
+    path('movies/', MovieListCreateView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-datail-view')
 ]
