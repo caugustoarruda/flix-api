0d4f022c3571f536cc5530cd4a3a09d91589a9ab
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-28 13:20:42 -0300

    URLs version

---
+++ movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('movies/', views.MovieListCreateView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='movie-datail-view'),
+]
