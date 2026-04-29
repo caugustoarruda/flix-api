323fc4defb2392592ad987100d38570c794288f7
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-29 16:18:40 -0300

    Test validate serializer

--- movies/serializer.py
+++ movies/serializer.py
@@ -7,3 +7,8 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("Data de lançamento nao pode ser inferior a 1990.")
+        return value
