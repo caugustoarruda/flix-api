bb2577425276da8cd163aeb0fd86e5b961e11112
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-30 09:37:16 -0300

    Calculate rate brute force

--- movies/serializer.py
+++ movies/serializer.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from movies.models import Movie
+from reviews.models import Review
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    # is a calculated field
+    rate = serializers.SerializerMethodField(read_only=True)
+
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+
+        stars_ = 0
+        for review in reviews:
+            stars_ += review.stars
+        return stars_/len(reviews)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError("Data de lançamento nao pode ser inferior a 1990.")
         return value
