2b5b67e8db4ca53cca09ba7a10fc4eb58ee78fc7
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-02 13:47:59 -0300

    Urls authenticated

--- actors/views.py
+++ actors/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
 from actors.models import Actor
 from actors.serializer import ActorSerializer
 
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
