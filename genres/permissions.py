845e9fe1ec6614800fd23f8f8a132be19120eb29
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-04 10:47:47 -0300

    Finishing genre permissions

--- genres/permissions.py
+++ genres/permissions.py
@@ -6,4 +6,12 @@ class GenrePermissionClass(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_superuser:
             return True
+        elif request.method in ['GET', 'OPTIONS', 'HEAD']:
+            return request.user.has_perm('genres.view_genre')
+        elif request.method == 'POST':
+            return request.user.has_perm('genres.add_genre')
+        elif request.method in ['PUT',  'PATCH']:
+            return request.user.has_perm('genres.change_genre')
+        elif request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
         return False
