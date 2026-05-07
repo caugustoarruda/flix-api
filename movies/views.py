76158251abac65e0b1ad5e55d9404e62b1319229
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-07 11:41:02 -0300

    Stats endpoint

--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,39 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializer import MovieSerializer
+from app.permissions import GlobalPermissionClass
+from django.db.models import Count, Avg
+from reviews.models import Review
 
 
 class MovieListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, GlobalPermissionClass)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, GlobalPermissionClass)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalPermissionClass)
+    queryset = Movie.objects.all()
+    
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.all().count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        return response.Response(
+            data={
+                'total_movies': total_movies,
+                'movie_by_genre': movies_by_genre,
+                'total_reviews': total_reviews,
+                'average_stars': round(average_stars, 1) if average_stars else 0
+                },
+            status=status.HTTP_200_OK
+        )
